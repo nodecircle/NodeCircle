c42bd23b7df725b8443fe8f3f776509dfb6fafde
NodeCircle <admin@nodecircle.io>
2018-06-28 10:13:14 -0700

    Update masternode collateral to 50000

--- src/wallet.h
+++ src/wallet.h
@@ -79,9 +79,9 @@ enum WalletFeature {
 enum AvailableCoinsType {
     ALL_COINS = 1,
     ONLY_DENOMINATED = 2,
-    ONLY_NOT10000IFMN = 3,
-    ONLY_NONDENOMINATED_NOT10000IFMN = 4, // ONLY_NONDENOMINATED and not 10000 NCX at the same time
-    ONLY_10000 = 5,                        // find masternode outputs including locked ones (use with caution)
+    ONLY_NOT50000IFMN = 3,
+    ONLY_NONDENOMINATED_NOT50000IFMN = 4, // ONLY_NONDENOMINATED and not 10000 NCX at the same time
+    ONLY_50000 = 5,                        // find masternode outputs including locked ones (use with caution)
     STAKABLE_COINS = 6                          // UTXO's that are valid for staking
 };
 
